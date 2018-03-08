5d2ecac7641665762d5153269c4e9e58da87564f
Michael LoTurco <mikeloturco13@gmail.com>
2018-03-08 12:54:46 -0500

    refactor as functional component (quality)

--- lms/djangoapps/support/static/support/jsx/entitlements/entitlements.jsx
+++ lms/djangoapps/support/static/support/jsx/entitlements/entitlements.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 
-export class EntitlementSupportPage extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-  render() {
-    return (
-      <div>
-        Base Entitlement Support Page
-      </div>
-    );
-  }
+export function EntitlementSupportPage(props) {
+  return (
+    <div>
+      Base Entitlement Support Page
+    </div>
+  );
 }
